b56cc87bd59d05980e172cafcb4ca70ee3969127
Alex Carvalho <alex-carvalho@users.noreply.github.com>
2025-04-29 15:01:56 +0000

    Add files

--- samples/java/spring-web-3-j21/src/main/java/com/ac/demo/HelloController.java
+++ samples/java/spring-web-3-j21/src/main/java/com/ac/demo/HelloController.java
@@ -2,12 +2,17 @@ package com.ac.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloController {
 
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public String hello() {
+        logger.info("Hello endpoint was called");
         return "Hello World";
     }
 }
