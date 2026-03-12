1b112948a857a3c3c6614b2d238efe8f9d5c677e
Alex Carvalho <alexcarvalhoandres@gmail.com>
2026-03-12 10:16:25 -0300

    Add file

--- samples/rust/basic/rust_features/src/features/mod.rs
+++ samples/rust/basic/rust_features/src/features/mod.rs
@@ -1,7 +1,9 @@
 pub mod loops;
 pub mod ownership;
+pub mod strings;
 
 pub fn run_examples() {
     loops::run();
     ownership::run();
+    strings::run();
 }
---
+++ samples/rust/basic/rust_features/src/features/strings.rs
@@ -0,0 +1,19 @@
+pub fn run() {
+    // stored on the program's binary, immutable, and has a fixed size
+    let simple = "hello"; // type is &str
+    println!("simple string literal: {simple}");
+
+    let mut simple_mut = "hello"; // this is still a string literal, but we can reassign the variable to point to a different string literal
+    simple_mut = "hello rust";
+    println!("simple mut string literal: {simple_mut}");
+
+    // stored on the heap, mutable, and can grow in size
+    let mut greeting = String::from("hello"); // type is String
+    greeting.push_str(" rust");
+    println!("string demo: {greeting}");
+
+    // any diferente way to create a new string?
+    let name = "world".to_string(); // this is a method on the &str type that creates a String
+    println!("name: {name}");
+
+}
