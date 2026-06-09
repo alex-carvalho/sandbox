db4ee3cf69161c346b074b6a63d6b8708b477cb9
Alex Carvalho <alexcarvalhoandres@gmail.com>
2026-06-09 11:34:55 -0300

    Add file

--- clean-space.sh
+++ clean-space.sh
@@ -1,12 +1,9 @@
-#Clean Codespace
+#Clean space
 
 docker system prune -a --volumes -f
 
 npm cache clean --force
 yarn cache clean
-
-
 pip cache purge
 
-sudo apt-get clean
-sudo apt-get autoremove -y
+git clean -fd
